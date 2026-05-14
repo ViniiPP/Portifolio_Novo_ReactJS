6511abde4d06d0939ec9622a0ea9c1ab365a5700
ViniiPP <viniciusppolli07@gmail.com>
2026-05-14 12:57:19 -0300

    Fix: Deploy

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { projectsData } from './Data';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
